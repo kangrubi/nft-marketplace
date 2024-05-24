38f91c212e903e3ad4a27e9ae7670995fbeec580
kangrubi <rubi0624@nate.com>
2024-05-24 19:03:08 +0900

    ✨ (index.js): react-dom/client의 createRoot를 사용하여 루트 엘리먼트 렌더링으로 변경 ♻️ (index.js): 불필요한 코드 및 주석 제거

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './frontend/components/App';
-import * as serviceWorker from './serviceWorker';
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import { RouterProvider } from "react-router-dom";
+import { rootRouter } from "./frontend/router/root-router";
 
 const rootElement = document.getElementById("root");
-render( <App />, rootElement);
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = createRoot(rootElement);
+root.render(<RouterProvider router={rootRouter} />);
