081469fe3d8fa28613b319db8e90f9ad2a512857
kangrubi <rubi0624@nate.com>
2024-05-24 19:04:07 +0900

    ✨ (App.js): react-router-dom의 Outlet 및 Navbar 컴포넌트 추가 📝 (App.js): Spinner 및 Metamask 연결 대기 메시지 추가 🔧 (App.js): window.etherum -> window.ethereum 오타 수정 💡 (App.js): account 변수에 대한 console.log 추가 ⬆️ (App.js): useEffect를 사용하여 account 상태 변경 시 console.log 추가

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.png";
 import "./App.css";
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MarketplaceAbi from "../contractsData/Marketplace.json";
 import MarketplaceAddress from "../contractsData/Marketplace-address.json";
 import NFTAbi from "../contractsData/NFT.json";
 import NFTAddress from "../contractsData/NFT-address.json";
+import { Outlet } from "react-router-dom";
+import Navigation from "./Navbar";
+import { Spinner } from "react-bootstrap";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -15,10 +18,14 @@ function App() {
 
   const web3Handler = async () => {
     // MetaMask를 통해 계정 요청
-    const accounts = await window.etherum.request({
+    const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
     setAccount(accounts[0]);
+
+    console.log(account);
+
+    if (!account) return;
 
     // MetaMask에서 provider 가져오기
     const provider = new ethers.providers.Web3Provider(window.ethers);
@@ -42,49 +49,33 @@ function App() {
     setLoading(false);
   };
 
+  useEffect(() => {
+    console.log(account);
+  }, [account]);
+
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Navigation web3Handler={web3Handler} account={account} />
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "80vh",
+          }}
         >
-          Dapp University
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo" />
-              </a>
-              <h1 className="mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to
-                reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN{" "}
-                <u>
-                  <b>NOW! </b>
-                </u>
-              </a>
-            </div>
-          </main>
+          <Spinner animation="border" style={{ display: "flex" }} />
+          <p className="mx-3 my-0">Awaiting Metamask Connection...</p>
         </div>
-      </div>
+      ) : (
+        <Outlet
+          context={{
+            marketplace,
+            nft,
+          }}
+        />
+      )}
     </div>
   );
 }
