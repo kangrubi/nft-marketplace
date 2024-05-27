f455b8c306eb7abee3347befbab7209bc7f68529
kangrubi <rubi0624@nate.com>
2024-05-27 18:04:30 +0900

    ✨ (Home.js): axios 및 ethers 라이브러리 추가, 마켓플레이스 아이템 로딩 및 구매 기능 추가, 홈 컴포넌트 UI 업데이트

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,12 +1,94 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row, Button } from "react-bootstrap";
 import { useOutletContext } from "react-router-dom";
+import { ethers } from "ethers";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const { marketplace, nft } = useOutletContext();
 
-  console.log(marketplace);
+  const loadMarketplaceItems = async () => {
+    const itemCount = await marketplace.itemCount();
 
-  return <div></div>;
+    if (itemCount) {
+      for (let i = 1; i <= itemCount; i++) {
+        const item = await marketplace.items(i);
+
+        if (!item.sold) {
+          const uri = await nft.tokenURI(item.tokenURI);
+
+          const response = await axios.get(uri);
+
+          if (response.data) {
+            const metadata = response.data;
+
+            const totalPrice = await marketplace.getTotalPrice(item.itemId);
+
+            setItems({
+              totalPrice,
+              itemId: item.itemId,
+              seller: item.seller,
+              name: metadata.name,
+              description: metadata.description,
+              image: metadata.image,
+            });
+          }
+        }
+      }
+    }
+
+    setLoading(false);
+  };
+
+  const buyMarketItem = async (item) => {
+    await (
+      await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })
+    ).wait();
+  };
+
+  useEffect(() => {
+    loadMarketplaceItems();
+  }, []);
+
+  return (
+    <div>
+      {items.length > 0 ? (
+        <div className="px-5 container">
+          <Row xs={1} md={2} lg={4} className="g-4 py-5">
+            {items.map((item, i) => (
+              <Col key={i}>
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Body color="secondary">
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <div className="d-grid">
+                      <Button
+                        onClick={() => buyMarketItem(item)}
+                        variant="primary"
+                        size="lg"
+                      >
+                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+                      </Button>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      ) : (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>No listed assets</h2>
+        </main>
+      )}
+    </div>
+  );
 };
 
 export default Home;
