f93125796e433e20aea2e57b033b9c363daffae3
kangrubi <rkdfnql22@gmail.com>
2024-05-28 11:28:59 +0900

    ✨ (Create.js): Replace Helia client with ipfsHttpClient for file upload functionality 🔧 (Create.js): Add IPFS configuration with authorization headers using project credentials from environment variables

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import { createHelia } from "helia";
-
-// const client = createHelia();
+import { create as ipfsHttpClient } from "ipfs-http-client";
 
 const Create = () => {
   const [image, setImage] = useState("");
   const [price, setPrice] = useState(null);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+
+  const projectId = process.env.REACT_APP_PROJECT_ID;
+  const projectAPIKey = process.env.REACT_APP_API_KEY;
+
+  const authorization = "Basic " + btoa(projectId + ":" + projectAPIKey);
+  const ipfs = ipfsHttpClient({
+    url: "https://ipfs.infura.io:5001/api/v0",
+    headers: {
+      authorization,
+    },
+  });
+
+  console.log(ipfs);
 
   const uploadToIPFS = async (event) => {
     event.preventDefault();
